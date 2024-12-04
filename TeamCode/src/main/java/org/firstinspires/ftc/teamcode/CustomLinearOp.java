53ca9a3564b3682ec09da309bfc744a7d51b5d24
Eric Zhang <ericbkzhang@gmail.com>
2024-12-04 11:42:58 -0500

    Remove initial angle from initialization

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CustomLinearOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CustomLinearOp.java
@@ -127,7 +127,6 @@ public class CustomLinearOp extends LinearOpMode {
         FoldingArm.RotationRange rotationRange = new FoldingArm.RotationRange(
                 Integer.MIN_VALUE,
                 Integer.MAX_VALUE, // 1080
-                -40,
                 MotorType.TETRIX_TORQUENADO.getTicksPerRotation()
                         / 360.0
                         * rotationGearRatio
@@ -137,7 +136,6 @@ public class CustomLinearOp extends LinearOpMode {
         FoldingArm.FoldingRange foldingRange = new FoldingArm.FoldingRange(
                 Integer.MIN_VALUE,
                 Integer.MAX_VALUE,
-                160,
                 MotorType.TETRIX_TORQUENADO.getTicksPerRotation()
                         / 360.0
                         * foldingGearRatio
