8d2d31058e611e785c5e0615e4080a2c5bfe389c
Eric Zhang <ericbkzhang@gmail.com>
2024-12-20 14:30:57 -0500

    Remove RoadRunner directories from teamcode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.
 
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 
