a19ba9528dfafe89ba3b63971ed0c7a745434773
Eric Zhang <ericbkzhang@gmail.com>
2025-05-30 19:10:37 -0400

    Experiment with trajectories

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -1,10 +1,17 @@
 package org.firstinspires.ftc.teamcode;
 
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.roadrunner.Arclength;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Pose2dDual;
+import com.acmerobotics.roadrunner.PosePath;
+import com.acmerobotics.roadrunner.Trajectory;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.TrajectoryBuilder;
+import com.acmerobotics.roadrunner.TrajectoryBuilderParams;
+import com.acmerobotics.roadrunner.VelConstraint;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-
-import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
-
-import java.util.List;
 
 @Autonomous(name = "Auto")
 public class Auto extends CustomLinearOp {
@@ -14,6 +21,12 @@ public class Auto extends CustomLinearOp {
     @Override
     public void runOpMode() {
         super.runOpMode();
+
+        new TrajectoryBuilder(new Pose2d(0, 0, 0))
+                .forward(40)
+                        .build();
+
+        MECANUM_DRIVE.actionBuilder(new Pose2d(0, 0, 0));
 
         telemetry.update();
     }
